aecab28b0085dde13bef613d425ea92e022214b6
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-03-20 17:22:58 +0100

    build helper files

--- api/projects/projectsHelper.js
+++ api/projects/projectsHelper.js
@@ -1,49 +1,112 @@
 const db = require("../../data/db-config");
 
 module.exports = {
-  find,
-  findById,
-  findSteps,
-  add,
-  addStep,
-  update,
-  remove
+  findProjects,
+  findProjectById,
+  findProjectResources,
+  findProjectTasks,
+  addProject,
+  addProject,
+  updateProject,
+  removeProject,
+  findTasks,
+  findTaskById,
+  addTask,
+  addTask,
+  updateTask,
+  removeTask,
+  findResources,
+  findResourceById,
+  findResourceProjects,
+  addResource,
+  addResource,
+  updateResource,
+  removeResource,
 }
 
-function find() {
-  ""
+function findProjects() {
   return db("projects")
 }
 
-function findById(id) {
+function findProjectById(id) {
   return db("projects").where({ id }).first()
 }
 
-function findSteps(id) {
+function findProjectResources(id) {
   return db.select("*")
-    .from("projects")
-    .join("steps", "projects.id", "=", "steps.project_id")
+    .from("resources")
+    .join("project_resources", "resources.id", "=", "project_resources.project_id")
     .where("project_id", "=", id)
 }
 
-function add(projectData) {
+function findProjectTasks(id) {
+  return db.select("*")
+    .from("tasks")
+    .where("project_id", "=", id)
+}
+
+function addProject(projectData) {
   return db("projects").insert(projectData)
 }
 
-function addStep(stepData, id) {
-  return db("steps").insert({
-    ...stepData,
-    project_id: id
-  })
-}
-
-function update(changes, id) {
+function updateProject(changes, id) {
   return db("projects")
     .where({ id })
     .update(changes)
 }
 
-function remove(id) {
+function removeProject(id) {
   return db("projects").where("id", "=", id).del()
 }
 
+function findTasks() {
+  return db("tasks")
+}
+
+function findTaskById(id) {
+  return db("tasks").where({ id }).first()
+}
+
+function addTask(taskData) {
+  return db("tasks").insert({ taskData })
+}
+
+function updateTask(changes, id) {
+  return db("tasks")
+    .where({ id })
+    .update(changes)
+}
+
+function removeTask(id) {
+  return db("projects").where("id", "=", id).del()
+}
+
+function findResources() {
+  return db("resources")
+}
+
+function findResourceById(id) {
+  return db("resources").where({ id }).first()
+}
+
+function findResourceProjects(id) {
+  return db.select("*")
+    .from("projects")
+    .join("project_resources", "projects.id", "=", "project_resources.resource_id")
+    .where("resource_id", "=", id)
+}
+
+
+function addResource(resourceData) {
+  return db("resources").insert({ taskData })
+}
+
+function updateResource(changes, id) {
+  return db("resources")
+    .where({ id })
+    .update(changes)
+}
+
+function removeResource(id) {
+  return db("resources").where("id", "=", id).del()
+}
